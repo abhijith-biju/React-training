ac5cf7f992bddd505c8f90ae7e8ccbfbe3ca91b1
Abhijith Biju <abhijith.biju@qburst.com>
2023-11-01 18:27:24 +0530

    added conditional rendering and dynamic lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+
 import Header from './components/Header/Header.jsx';
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import CoreConcepts from './components/CoreComponents.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState();
+
+    function handleClick(selectedTopic) {
+        setSelectedTopic(selectedTopic);
+    }
+
+    let tabContent = <p>Please select a topic.</p>;
+
+    if (selectedTopic) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+            </div>
+        );
+    }
+
     return (
         <div>
             <Header />
@@ -11,19 +33,64 @@ function App() {
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcepts {...CORE_CONCEPTS[0]} />
-                        <CoreConcepts {...CORE_CONCEPTS[1]} />
-                        <CoreConcepts {...CORE_CONCEPTS[2]} />
-                        <CoreConcepts {...CORE_CONCEPTS[3]} />
+                        {CORE_CONCEPTS.map((conceptItem, key) => (
+                            <CoreConcepts {...conceptItem} key={key} />
+                        ))}
                     </ul>
                 </section>
                 <section id="examples">
                     <menu>
-                        <TabButton>Components</TabButton>
-                        <TabButton>JSX</TabButton>
-                        <TabButton>Props</TabButton>
-                        <TabButton>States</TabButton>
+                        <TabButton
+                            isSelected={selectedTopic === 'components'}
+                            onSelect={() => handleClick('components')}
+                        >
+                            Components
+                        </TabButton>
+                        <TabButton
+                            isSelected={selectedTopic === 'jsx'}
+                            onSelect={() => handleClick('jsx')}
+                        >
+                            JSX
+                        </TabButton>
+                        <TabButton
+                            isSelected={selectedTopic === 'props'}
+                            onSelect={() => handleClick('props')}
+                        >
+                            Props
+                        </TabButton>
+                        <TabButton
+                            isSelected={selectedTopic === 'state'}
+                            onSelect={() => handleClick('state')}
+                        >
+                            States
+                        </TabButton>
+
+                        {/* ERROR */}
+
+                        {/* {CORE_CONCEPTS.map((conceptItem, index) => (
+                            <TabButton
+                                isSelected={selectedTopic === conceptItem.title}
+                                onSelect={() => handleClick(conceptItem.title)}
+                                key={index}
+                            >
+                                JSX
+                            </TabButton>
+                        ))} */}
                     </menu>
+                    {tabContent}
+
+                    {/* Alternative method to conditionally render */}
+
+                    {/* {!selectedTopic && <p>Please select a topic</p>}
+                    {selectedTopic && (
+                        <div id="tab-content">
+                            <h3>{EXAMPLES[selectedTopic].title}</h3>
+                            <p>{EXAMPLES[selectedTopic].description}</p>
+                            <pre>
+                                <code>{EXAMPLES[selectedTopic].code}</code>
+                            </pre>
+                        </div>
+                    )} */}
                 </section>
             </main>
         </div>
